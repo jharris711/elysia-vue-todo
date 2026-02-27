f9f208616148362a510593930f2565999a35031f
Josh Harris <js.harris711@gmail.com>
2026-02-26 23:52:08 -0500

    todo show/ui route

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
+import TodoView from '@/views/TodoView.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: HomeView },
+    { path: '/todos', redirect: '/'},
+    { path: '/todos/:id', component: TodoView},
   ],
 })
 
---
+++ ui/src/views/TodoView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script> 
+
+<template>
+    <div>To do view in vue: {{ $route.params.id }}</div>
+</template>
+
+<style scoped>
+</style>
